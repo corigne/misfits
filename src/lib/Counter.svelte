7fd31479ea6199af9d56baca998da6b515da8d47
Nathan Jodoin <corigne@gmail.com>
2023-05-05 22:58:44 +0000

    Initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let count: number = parseInt(localStorage.getItem("click_count"));
+
+  if (!count) {
+    count = 0;
+  }
+
+  const increment = () => {
+    count += 1;
+    localStorage.setItem("click_count", count);
+  };
+
+  const load_count = () => {
+    count = localStorage.getItem("click_count");
+  };
+</script>
+
+<button on:click={increment} on:load={load_count}>
+  count is {count}
+</button>
