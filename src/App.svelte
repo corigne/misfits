8ae3762743ffc466b2b59717b8915441d0cf12d3
Nathan Jodoin <corigne@gmail.com>
2023-05-24 01:12:47 +0000

    Added more carp.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
+  import carp from './assets/carp.jpg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
+  
+  <div>
+    <h1>
+      Under Construction...
+    </h1>
+    <img src={carp} alt="An image of a carp.">
+  </div>
+
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
@@ -19,13 +28,6 @@
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
