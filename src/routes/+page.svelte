c76078fbf67555a205dc379155f75c6522e1059d
Nathan Jodoin <corigne@gmail.com>
2024-05-07 11:17:06 -0500

    Added carp.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="content">
+    <h1>Under Construction</h1>
+    <img src="carp.jpg" alt="A carp being held."/>
+    <p>
+        Coming soon!
+    </p>
+</div>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+    p {
+        text-align: center;
+    }
+    .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    img {
+        width: 700px;
+        margin: auto;
+    }
+</style>
