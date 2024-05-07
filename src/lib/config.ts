f3a6fb5c9042b026b7b28d5cddb33aaf76953e22
Nathan Jodoin <corigne@gmail.com>
2024-05-07 10:46:20 -0500

    Added Hack font-family and updated eslint config.

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment";
+
+export const title = "nathan jodoin :: devblog";
+export const description = "devblog and resume site made with SvelteKit";
+export const url = dev ? "http://localhost:5173/" : "https://dev.misfits.rip";
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <style>
+    @import url("//cdn.jsdelivr.net/npm/hack-font@3.3.0/build/web/hack.css");
+  </style>
+</svelte:head>
+
+<slot />
+
+<style>
+  * {
+    font-family: "Hack", monospace;
+  }
+</style>
