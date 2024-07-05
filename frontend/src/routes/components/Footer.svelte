48f82baac55d18259b822170e97eeecdd45cbf52
Nathan Jodoin <corigne@gmail.com>
2024-07-05 12:14:44 -0500

    Adjusted footer style to make top border match text color.

--- frontend/src/routes/components/Footer.svelte
+++ frontend/src/routes/components/Footer.svelte
@@ -2,6 +2,6 @@
     import * as config from '$lib/config'
 </script>
 
-<footer class="border-t my-2 pt-2 text-overlay0">
+<footer class="border-t border-overlay1 my-2 pt-2 text-overlay1">
     <p>{config.title} &copy {new Date().getFullYear()}</p>
 </footer>
